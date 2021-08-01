fb667b7d20fb6b1bfa748ebcf91f2c2fec1215bf
NamineRae <maddy.daynes@gmail.com>
2021-07-31 19:25:20 -0600

    Got desired data selected and displaying in html!

--- scripts.js
+++ scripts.js
@@ -18,10 +18,30 @@
 
 //I think this is how you link an api?
 
-fetch(https://db.ygoprodeck.com/api/v7/cardinfo.php')
+fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
     .then((resObject) => {
         return resObject.json()
     })
     .then((json) => {
-        console.log(json)
+        console.log(json.data)
+        displayApiData(json.data)
     })
+
+// var defense = "def"
+// var stregnth = "atk"
+// var name = "name"
+
+function displayApiData(data) {
+    let dataEl = document.querySelector('#data');
+    data.forEach(card => {
+        let h2 = document.createElement('h2');
+        let h2TExt = document.createTextNode(card.name)
+        let h2TExt1 = document.createTextNode(card.race)
+        let h2TExt2 = document.createTextNode(card.type)
+        h2.append(h2TExt)
+        h2.append(h2TExt1)
+        h2.append(h2TExt2)
+        dataEl.append(h2)  
+    });
+    
+}
